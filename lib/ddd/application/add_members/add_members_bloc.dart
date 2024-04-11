7f8c8e271413c16c929004eee966fb8d759a374d
medizttech <mediezytech@gmail.com>
2024-04-11 11:03:36 +0530

    changes

--- lib/ddd/application/add_members/add_members_bloc.dart
+++ lib/ddd/application/add_members/add_members_bloc.dart
@@ -3,7 +3,8 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
-import 'package:mediezy_user/ddd/domain/error_model/error_model.dart';
+import 'package:mediezy_user/ddd/domain/errors/errores_model/errores_model.dart';
+
 import '../../domain/add_member/add_member_service.dart';
 import '../../domain/add_member/model/add_member_model.dart';
 part 'add_members_event.dart';
@@ -12,43 +13,76 @@ part 'add_members_bloc.freezed.dart';
 
 @injectable
 class AddMembersBloc extends Bloc<AddMembersEvent, AddMembersState> {
-  final AddMemberRepo addMemberRepo;
-  AddMembersBloc(this.addMemberRepo) : super(AddMembersState.initial()) {
-    on<_Started>((event, emit) async {
-      emit(
-        const AddMembersState(
-            isloding: true, isError: false, message: "", status: false),
-      );
-      log(emit.toString());
-      final addMemberResult = await addMemberRepo.addFamilyMember(
-        event.fullName,
-        event.age,
-        event.mobileNumber,
-        event.gender,
-        event.regularMedicine,
-        event.surgeryName,
-        event.treatmentTaken,
-        event.surgeryDetails,
-        event.treatmentTakenDetails,
-        event.allergies,
-        event.medicines,
-        event.context,
-      );
+  
+  final AddMemberRepo _addMemberRepo;
+  AddMembersBloc(this._addMemberRepo) : super(AddMembersState.initial()) {
+     on<AddMembersEvent>((event, emit) async {
 
-      final state = addMemberResult.fold((ErrorModel error) {
-        return AddMembersState(
-            isloding: false,
-            isError: true,
-            message: error.message!,
-            status: false);
-      }, (ClintClinicModelData? success) {
-        return AddMembersState(
-            isloding: false,
-            isError: false,
-            message: success!.message!,
-            status: success.status!);
-      });
-      emit(state);
+emit(AddMembersState.initial());
+final _result =await _addMemberRepo.getdoctersData(event.fullName,
+            event.age,
+            event.mobileNumber,
+             event.gender,
+              event.regularMedicine,
+             event.surgeryName,
+            event.treatmentTaken,
+               event.surgeryDetails,
+          event.treatmentTakenDetails,
+       event.allergies,
+           event.medicines,
+             event.context,);
+             final _state =_result.fold((ErroresModel l) {
+              return  AddMembersState(isloding: false,
+               isError: true, message: l.message!, status: false);
+             },
+              (ClintClinicModelData? r) {
+                return AddMembersState(isloding: false,
+                 isError: false, 
+                 message: r!.message!, status: r.status!);
+
+              });
+emit(_state);
+
+      // emit(state.copyWith(
+      //     isloding: true, registerFaileurOrSuccessOption: none()));
+      // log(emit.toString());
+      // final Either<MainFailure, ClintClinicModelData?> addMemberOption =
+      //     await addMemberRepo.getdoctersData(
+      //         event.fullName,
+      //       event.age,
+      //       event.mobileNumber,
+      //        event.gender,
+      //         event.regularMedicine,
+      //        event.surgeryName,
+      //       event.treatmentTaken,
+      //          event.surgeryDetails,
+      //     event.treatmentTakenDetails,
+      //  event.allergies,
+      //      event.medicines,
+      //        event.context,
+      //         );
+      // log("${addMemberOption.toString()} ======");
+      // emit(
+      //   addMemberOption.fold(
+      //     (failure) => state.copyWith(
+      //       isloding: true,
+      //       registerFaileurOrSuccessOption: some(
+      //         left(
+      //           failure,
+      //         ),
+      //       ),
+      //     ),
+      //     (success) => state.copyWith(
+      //       isloding: false,
+      //       model: success,
+      //       registerFaileurOrSuccessOption: some(
+      //         right(
+      //           success!,
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // );
     });
   }
 }
