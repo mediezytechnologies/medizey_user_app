73b8224fd17cc07c3f8ef2ea357652fecc076bfc
mediezytechnologies <mediezytech@gmail.com>
2024-05-14 10:13:53 +0530

    change

--- lib/Ui/Screens/demo/google_auth_demo.dart
+++ lib/Ui/Screens/demo/google_auth_demo.dart
@@ -7,8 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-import 'package:flutter_signin_button/button_list.dart';
-import 'package:flutter_signin_button/button_view.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
