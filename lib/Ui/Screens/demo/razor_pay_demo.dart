699182252c3fef2cb4e83ae7a1532e01a8966c4e
Akber A A <165126011+mediezytechnologies@users.noreply.github.com>
2024-05-22 17:40:04 +0530

    android payment integration done ios pending

--- lib/Ui/Screens/demo/razor_pay_demo.dart
+++ lib/Ui/Screens/demo/razor_pay_demo.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:mediezy_user/Ui/Services/general_services.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
@@ -11,11 +10,63 @@ class RazorPayDemo extends StatefulWidget {
 }
 
 class _RazorPayDemoState extends State<RazorPayDemo> {
-  Razorpay razorPay = Razorpay();
+  late Razorpay _razorpay;
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  void openCheckout() {
+    var options = {
+      'key': 'rzp_live_VUHG1603ofFkWt',
+      'amount': 100,
+      'name': 'Mediezy Technologies',
+      'description': 'Platform fee',
+      'prefill': {'contact': '9744887799', 'email': 'mediezytech@gmail.com'},
+      'external': {
+        'wallets': ['paytm']
+      },
+      'theme': {'color': '#595DE50'},
+      'modal': {'backdropclose': true, 'confirm_close': true},
+      'retry': {'enabled': true, 'max_count': 4},
+      'timeout': 300
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    GeneralServices.instance
+        .showToastMessage("Payment succesfull${response.paymentId}");
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    GeneralServices.instance
+        .showToastMessage("Payment failed${response.message}");
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    GeneralServices.instance
+        .showToastMessage("External wallet${response.walletName}");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    razorPay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
-    razorPay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Razor Pay"),
@@ -24,41 +75,11 @@ class _RazorPayDemoState extends State<RazorPayDemo> {
       body: Center(
         child: OutlinedButton(
           onPressed: () {
-            var options = {
-              'key': 'rzp_live_VUHG1603ofFkWt',
-              'amount': 100,
-              'name': 'Mahesh',
-              'description': 'Platform fee',
-              'prefill': {
-                'contact': '9744887799',
-                'email': 'mediezytech@gmail.com'
-              }
-            };
-            razorPay.open(options);
+            openCheckout();
           },
           child: const Text("Pay 10 rupees"),
         ),
       ),
     );
-  }
-
-  void _handlePaymentSuccess(PaymentSuccessResponse response) {
-    GeneralServices.instance.showToastMessage("Payment success");
-    log(response.paymentId.toString());
-  }
-
-  void _handlePaymentError(PaymentFailureResponse response) {
-    GeneralServices.instance.showToastMessage("Payment failed");
-    log(response.message.toString());
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    try {
-      razorPay.clear();
-    } catch (e) {
-      log(e.toString());
-    }
   }
 }
